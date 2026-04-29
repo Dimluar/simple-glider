45681acf404c611968154cc9e7f1637767014e09
Dimluar <148585902+Dimluar@users.noreply.github.com>
2026-04-29 13:24:37 +0200

    Add glider

---
+++ src/main/java/io/github/dimluar/simpleglider/item/ModItems.java
@@ -0,0 +1,26 @@
+package io.github.dimluar.simpleglider.item;
+
+import io.github.dimluar.simpleglider.SimpleGlider;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item GLIDER = register("glider", new Item(new Item.Settings()));
+
+
+    private static Item register(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(SimpleGlider.MOD_ID, name), item);
+    }
+
+    public static void initialize() {
+        SimpleGlider.LOGGER.info("Registering Items for " + SimpleGlider.MOD_ID + "!");
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries -> {
+            entries.add(GLIDER);
+        });
+    }
+}
